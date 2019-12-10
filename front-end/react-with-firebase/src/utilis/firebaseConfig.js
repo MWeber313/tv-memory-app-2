b2c81003b28d5e9d8b1bd604c03d2f14cb293c62
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-12-10 09:38:02 -0800

    User login with google complete

---
+++ front-end/react-with-firebase/src/utilis/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZztxm6LpSGppkDatC9Pp9N5c_nKP7KFY",
+    authDomain: "mw-beginner-firebase.firebaseapp.com",
+    databaseURL: "https://mw-beginner-firebase.firebaseio.com",
+    projectId: "mw-beginner-firebase",
+    storageBucket: "mw-beginner-firebase.appspot.com",
+    messagingSenderId: "124267493509",
+    appId: "1:124267493509:web:dcd5acaf4953b37463d1ec",
+    measurementId: "G-G1ZPK8RT58"
+};
+
+export default firebaseConfig;
